b9be16291d606dfcee65e886bcc6fec0501ff8e0
KostaZ <76644544+KostaZx@users.noreply.github.com>
2024-04-16 18:08:24 +0300

    Update qbox.sql

--- qbox.sql
+++ qbox.sql
@@ -94,6 +94,7 @@ CREATE TABLE IF NOT EXISTS `occasion_vehicles` (
   `price` int(11) DEFAULT NULL,
   `description` longtext DEFAULT NULL,
   `plate` varchar(50) DEFAULT NULL,
+    
   `model` varchar(50) DEFAULT NULL,
   `mods` text DEFAULT NULL,
   `occasionid` varchar(50) DEFAULT NULL,
@@ -261,7 +262,7 @@ CREATE TABLE IF NOT EXISTS `ox_doorlock` (
 ) ENGINE=InnoDB;
 
 INSERT INTO `ox_doorlock` (`id`, `name`, `data`) VALUES
-	(1, 'vangelico_jewellery', '{"maxDistance":2,"groups":{"police":0},"doors":[{"model":1425919976,"coords":{"x":-631.9553833007813,"y":-236.33326721191407,"z":38.2065315246582},"heading":306},{"model":9467943,"coords":{"x":-630.426513671875,"y":-238.4375457763672,"z":38.2065315246582},"heading":306}],"state":1,"coords":{"x":-631.19091796875,"y":-237.38540649414063,"z":38.2065315246582},"hideUi":true}');
+	(1, 'vangelico_jewellery', '{"maxDistance":2,"groups":{"police":0},"doors":[{"model":1425919976,"coords":{"x":-631.9553833007813,"y":-236.33326721191407,"z":38.2065315246582},"heading":306},{"model":9467943,"coords":{"x":-630.426513671875,"y":-238.4375457763672,"z":38.2065315246582},"heading":306}],"state":1,"coords":{"x":-631.19091796875,"y":-237.38540649414063,"z":38.2065315246582},"hideUi":true}'),
     (2, 'BigBankThermite1', '{\"heading\":160,\"doors\":false,\"maxDistance\":2,\"hideUi\":true,\"groups\":{\"police\":0},\"coords\":{\"x\":251.85757446289063,\"y\":221.0654754638672,\"z\":101.83240509033203},\"model\":-1508355822,\"state\":1,\"autolock\":1800}'),
     (3, 'BigBankThermite2', '{\"coords\":{\"x\":261.3004150390625,\"y\":214.50514221191407,\"z\":101.83240509033203},\"autolock\":1800,\"maxDistance\":2,\"groups\":{\"police\":0},\"model\":-1508355822,\"doors\":false,\"hideUi\":true,\"heading\":250,\"state\":1}'),
     (4, 'BigBankLPDoor', '{\"coords\":{\"x\":256.3115539550781,\"y\":220.65785217285157,\"z\":106.42955780029297},\"autolock\":1800,\"maxDistance\":2,\"model\":-222270721,\"doors\":false,\"lockpick\":true,\"hideUi\":true,\"heading\":340,\"state\":1,\"lockpickDifficulty\":[\"hard\"]}'),
